f9ec7b8180614686fd1f0590796ccae6e2e4749e
Nathan Verni <npverni@gmail.com>
2016-02-08 13:36:50 -0500

    video 12

--- app/main.js
+++ app/main.js
@@ -11,7 +11,19 @@ const todos = (state = [], action) => {
           text: action.text,
           completed: false
         }
-      ]
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        };
+
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      });
     default:
       return state;
   }
@@ -41,6 +53,47 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: false
+    }
+  ];
+
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1,
+  };
+
+  const stateAfter= [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 
 console.log("😄😄😄 ALL TESTS PASS 😄😄😄");
